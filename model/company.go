53f91c470c8e5d8f0829a9d1c8cca3852dc4d764
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-02-23 20:00:48 -0300

    Add company model

---
+++ model/company.go
@@ -0,0 +1,12 @@
+
+package model
+
+type Company struct {
+	id      string  `json:"id,omitempty"`
+	name    string  `json:"title,omitempty"`
+	zip     string  `json:"message,omitempty"`
+	website string  `json:"message,omitempty"`
+}
+
+
+

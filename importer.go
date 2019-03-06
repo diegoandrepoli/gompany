deffd0ed1e4337b24490d4085f0cecfd48c74e36
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-03-06 08:25:46 -0300

    Remove unused structure

--- importer.go
+++ importer.go
@@ -7,14 +7,6 @@ import (
 	"github.com/diegoandrepoli/gompany/configs"
 )
 
-// Gompany structure
-type Company struct {
-	ID       int      `json:"id,omitempty"`
-	Name     string   `json:"name,omitempty"`
-	Zip      string   `json:"zip,omitempty"`
-	Website  string   `json:"website,omitempty"`
-}
-
 /**
  * Main importer
  * Execute importer from file catalog name and client file name
